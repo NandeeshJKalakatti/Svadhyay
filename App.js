871de593ceef1340a10d20f53e9f46ab318751c0
Nandeesh2222 <102343556+Nandeesh2222@users.noreply.github.com>
2023-01-29 23:05:17 +0530

    n

--- App.js
+++ App.js
@@ -3,10 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import Homepage from './src/homepage';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Homepage/>
+    
   );
 }
 
